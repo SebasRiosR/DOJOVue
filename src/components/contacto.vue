04a96a0c30dfba1cf1e61add4106a89fa919fc79
SebasRiosR <sebastian.riosr@udea.edu.co>
2017-10-21 12:30:02 -0500

    Terminado

--- src/components/contacto.vue
+++ src/components/contacto.vue
@@ -1,9 +1,22 @@
 <template>
+  <div>
   <h2>Contacto</h2>
+  <ul>
+    <li v-for="marca in marcas">{{ marca }}</li>
+  </ul>
+  </div>
 </template>
 
 <script>
+export default {
+data() {
+    return {
+      marcas: ["Renault", "Chevrolet", "Alfa"]
+    }
+  }
+}
 </script>
 
 <style>
 </style>
+
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,8 +1,26 @@
 <template>
+  <div>
   <h2>Home</h2>
+  <input v-model="message" placeholder="edit me" name="entrada">
+  <button v-on:click="Click">Tomar palabra</button>
+  <p>Message is: {{ message2 }}</p>
+  </div>
 </template>
 
 <script>
+export default {
+data (){
+    return {
+    	message: "",
+    	message2: ""
+    }
+  },
+  methods:{
+	Click() {
+		this.message2 = this.message
+	}
+}
+}
 </script>
 
 <style>
